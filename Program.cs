7b303768b4da882528c0aece3868441da05632bb
= <4n4.informatica@gmail.com>
2023-08-18 18:15:26 -0300

    Finaliza o programa Haikai.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Nos dias quotidianos");
+Console.WriteLine("É que se passam");
+Console.WriteLine("Os anos");
+
+
